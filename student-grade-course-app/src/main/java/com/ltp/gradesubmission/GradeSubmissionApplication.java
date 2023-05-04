2e2fdf3639c8ac4706a9998e5f99dd6f5a431bbe
MaSlo93 <129391417+MaSlo93@users.noreply.github.com>
2023-05-04 04:18:14 +0200

    Add files via upload

---
+++ student-grade-course-app/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -0,0 +1,18 @@
+package com.ltp.gradesubmission;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+
+
+}
